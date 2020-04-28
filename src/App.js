3b2460382929f44bfb429939ff116c9d85c1a8e2
khalil <khemeri>
2020-04-28 10:28:44 +0100

    Timer  convetion & countdown works succussfully

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class App extends Component {
    state = {
-      x : 0,
+      x : 0
     };
     seconds = 0;
     minutes = 0;
@@ -23,7 +23,7 @@ class App extends Component {
             <div id="affichage"><h3>{this.hours} Hour  {this.minutes} minutes  {this.seconds} seconds</h3></div>
             <div className="aff2">
             <button className="cont"onClick={this.countDown} >countdown</button>
-            <button className="stop">stop</button>
+            <button className="stop" onClick={this.stop}>stop</button>
             <button className="reset" onClick={this.reset}>reset</button>
             </div>
             </div>
@@ -34,28 +34,35 @@ class App extends Component {
 
 
   setx =() =>{
-    console.log("aywash");
-    this.setState({x : document.getElementById("chrono").value*1 });
-    console.log(this.state.x);
-    this.convert(this.state.x)
+    console.log("aywash this :",this);
+    let chrono = this.state.x = document.getElementById("chrono").value*1;
+    this.convert(this.state.x);
+    this.setState({x : chrono});
+    console.log("x  : ",this.state.x);
+
   }
   convert=(x)=>{
 
-    if ( x < 60 ) { this.seconds = x }else{
-        if ( x < 3600) { this.seconds = x % 60 ; this.minutes =  (x-x%60)/60}else{
+    if ( x < 60 ) { this.seconds = x ; this.minutes=0;this.hours=0}else{
+        if ( x < 3600) { this.seconds = x % 60 ; this.minutes =  (x-x%60)/60 ; this.hours=0;}else{
             this.seconds = x % 60 ; this.minutes = (x%3600-x%60)/60 ; this.hours = (x-x%3600)/3600 ;
         }
     }
+    console.log("convert is executed");
   }
   countDown=()=>{
-    this.y = setInterval(()=>{ this.setState({x : this.state.x -1 }) }, 1000) ;
+    this.y = setInterval(()=>{ if (this.state.x >0) {this.state.x = this.state.x - 1; this.convert(this.state.x) ;this.setState({x : this.state.x }) ;console.log("x : ",this.state.x);
+    }else{this.stop()}}, 1000) ;
+    console.log("countDown is executed");
   }
   stop=()=>{
-    clearInterval(this.y)
+    clearInterval(this.y);
+    console.log("stop is executed");
   }
   reset=()=>{
-    this.seconds=0; this.minutes=0; this.hours,  this.setState({x : 0 });
-    clearInterval(this.y)
+    this.seconds=0; this.minutes=0; this.hours = 0 ; this.setState({x : 0 });
+    clearInterval(this.y);
+    console.log("reset is executed");
   }
   
 }
